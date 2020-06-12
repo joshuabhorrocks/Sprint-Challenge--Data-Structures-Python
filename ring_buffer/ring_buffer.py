be33f9ddd9256c4e3091f1e18906d722c1cd0694
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-06-12 13:12:08 -0600

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
+import collections
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer = []
 
     def append(self, item):
-        pass
+        if len(self.buffer) == self.capacity:
+            self.buffer.pop(0)
+        self.buffer.append(item)
+
 
     def get(self):
-        pass
+        return self.buffer
